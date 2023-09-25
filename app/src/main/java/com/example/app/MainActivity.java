a73cfb123feaabc52cb678517fe49cfc870f0a2e
Riley Marsh <RileyMarsh1337@proton.me>
2023-09-26 00:50:53 +0700

    set bsky.app and css

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.CookieManager;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -18,13 +20,47 @@ public class MainActivity extends Activity {
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAppCacheEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
 
-        // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
 
-        // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        String csp = "default-src 'self' https://bsky.app; style-src 'self' https://gist.githubusercontent.com";
+        mWebView.loadUrl("javascript:window.csp='" + csp + "'");
+
+        android.util.Log.d("MyApp", "Running injection script");
+
+        // Load the remote web page
+        mWebView.loadUrl("https://bsky.app");
+
+        // Inject local CSS into the remote web page when it's finished loading
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                String js = "fetch('https://gist.githubusercontent.com/rimar1337/cf96ee982afe28854281193728240c41/raw/84d7aabccab70dd89cd2053586d7e34ad4557c08/style.css')" +
+                        ".then(response => response.text())" +
+                        ".then(css => {" +
+                        "    const style = document.createElement('style');" +
+                        "    style.innerHTML = css;" +
+                        "    document.head.appendChild(style);" +
+                        "    console.log('MyApp real wowza js CSS injected!');" +
+                        "});";
+                mWebView.evaluateJavascript(js, null);
+                android.util.Log.d("MyApp", "Injected CSS using JavaScript");
+                super.onPageFinished(view, url);
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -12,7 +12,7 @@ class MyWebViewClient extends WebViewClient {
         String hostname;
 
         // YOUR HOSTNAME
-        hostname = "example.com";
+        hostname = "bsky.app";
 
         Uri uri = Uri.parse(url);
         if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
